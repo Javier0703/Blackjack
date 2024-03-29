950277a98e1b1a304ee71b138b9ddd5e982c65ce
Javier Calvo <javiercalvoporro@gmail.com>
2024-03-29 23:48:09 +0100

    Inicializacion del reparto de cartas

--- backjack.py
+++ backjack.py
@@ -3,7 +3,6 @@
 """
 
 from externo import CartaBase, Estrategia, Mazo
-import random
 
 #Creacion de clases/funciones para el programa
 
@@ -15,7 +14,6 @@ class Carta(CartaBase):
    def values(self):
       numCartasPorPalo = 13
       palos = ["♠","♣","♥","♦"]
-      #Devuelve una lista con la informacion de la carta:
       #valorNumerico --> Peso de la carta (1-10)
       valorNumerico = (self.ind%numCartasPorPalo)+1
       #num --> Numero carta (1,4,10,J,K...)
@@ -30,48 +28,68 @@ class Carta(CartaBase):
             num = "K" 
       #Palo de la carta
       palo = palos[self.ind//numCartasPorPalo]
+
+      #Tupla con los detalles de la carta
       return (valorNumerico,num,palo)
 
-estrategia = Estrategia(Mazo.NUM_BARAJAS)
-mazo = Mazo(Carta, estrategia)
-
-#Modos de juego del BlackJack, apuestas y balance:
-balance,apuesta = 0,0
-tipoApuesta = [2,10,50]
-gameMode = ['J','A']
-msg = "*** BLACKJACK - PARADIGMAS DE PROGRAMACION 2023/24 ***\n¿Modo de ejecución? [J]uego [A]nálisis: "
-
-#Creacion de nuestras clases
+#Main
 
 def main():
+   #Modos de juego del BlackJack, inicializacion de variables: balance, tipos de apuesta...
+   balance = 0
+   tipoApuesta = [2,10,50]
+   gameMode = ['J','A']
+   msg = "*** BLACKJACK - PARADIGMAS DE PROGRAMACION 2023/24 ***\n¿Modo de ejecución? [J]uego [A]nálisis: "
    #Creacion del BlackJack
    r = input(msg).upper()
    if r in gameMode:
-
       gamesToPlay = 1
-      game = 1 
+      game = 1
 
-      #Modo de juego seleccionado
+      #Seleccion del numero de juegos si es modo Analisis:
       if r == "A":
          while True:
             gamesToPlay = input("¿Número de partidas?: ")
-            if gamesToPlay.isdigit():  # Verificar si la entrada es un número entero
+            if gamesToPlay.isdigit():
                gamesToPlay = int(gamesToPlay)
                break  
             else:
                print("Por favor, ingresa un número entero válido.")
-      
-      #Seleccionado los juegos en el caso de modo Analisis
+   
+      #Inicio del Juego
+               
+      #Creacion de la estrategia y del mazo a usar
       estrategia = Estrategia(Mazo.NUM_BARAJAS)
       mazo = Mazo(Carta, estrategia)
-      print(mazo.reparte().values())
-      print(mazo.reparte().values())
-      print(mazo.reparte().values())
-      print(mazo.reparte().values())
-      print(mazo.reparte().values())
-      print(mazo.reparte().values())
-      print(mazo.reparte().values())
-      print(mazo.reparte().values())
+
+      while game<=gamesToPlay:
+
+         #Numero de partida y pregunta de la apuesta
+         print(f"--- INICIO DE LA PARTIDA #{game} --- BALANCE = {balance} €")
+         apuestaStr= "[" + "] [".join(str(i) for i in tipoApuesta) + "]"
+         msg = "¿Apuesta? " + apuestaStr + " "
+
+         while True:
+            apIncorrecta = "Apuesta seleccionada incorrecta"
+            apuesta = input(msg)
+            if apuesta.isdigit():
+               apuesta = int(apuesta)
+               if apuesta in tipoApuesta:
+                  break
+               else:
+                  print(apIncorrecta)
+            else:
+               print(apIncorrecta)
+         
+         #Ya tenemos apuesta:
+         print("REPARTO INICIAL")
+         
+
+         game +=1
+         break
+         estrategia = Estrategia(Mazo.NUM_BARAJAS)
+         mazo = Mazo(Carta, estrategia)
+
 
 
    else:
